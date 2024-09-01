369f07ee0c7fabc75c12ffc32c1eb08da2ee1eee
nakungu-esther <nakunguesther044@gmail.com>
2024-09-01 12:33:10 +0300

    add changes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET request for the home page
 router.get('/', (req, res) => {
-    res.render('home_page'); // Ensure that 'home_page' view exists in your views directory
+    res.render('index'); // Ensure that 'home_page' view exists in your views directory
 });
 
 module.exports = router;
